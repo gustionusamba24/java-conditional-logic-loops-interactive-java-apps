9c2c7af8985884e0a75aa6310fbf8a011d1db135
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-10-07 20:23:31 +0700

    feat(coding-challenge-14): finish challenge part 14

--- CodingExercise/src/exercises/CE14NumberOfDaysInMonth.java
+++ CodingExercise/src/exercises/CE14NumberOfDaysInMonth.java
@@ -22,9 +22,7 @@ public class CE14NumberOfDaysInMonth {
         if ((month >= 1 && month <= 12) && (year >= 1 && year <= 9999)) {
             return switch (month) {
                 case 1, 3, 5, 7, 8, 10, 12 -> 31;
-                case 2 -> {
-                    yield isLeapYear(year) ? 29 : 28;
-                }
+                case 2 -> isLeapYear(year) ? 29 : 28;
                 default -> 30;
             };
         }
