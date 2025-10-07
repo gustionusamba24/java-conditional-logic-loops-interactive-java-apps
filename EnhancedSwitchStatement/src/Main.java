b65241e8379e4c0ae17c3cd0939c7cc1d677ea3f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-10-07 13:47:35 +0700

    feat(advanced-switch-statement-challenge): finish challenge

---
+++ EnhancedSwitchStatement/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+        printDayOfWeek(8);
+    }
+
+    public static void printDayOfWeek(int day) {
+        String dayOfWeek = switch (day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+
+        System.out.println(day + " stands for " + dayOfWeek);
+    }
+}
