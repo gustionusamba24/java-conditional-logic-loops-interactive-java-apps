124776068837274c1e209c556a5e1b65cf08c8fd
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-10-07 11:56:48 +0700

    feat(traditional-switch-statement-challenge): finish challenge

---
+++ TraditionalSwitchStatementChallenge/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        char switchValue = 'E';
+        switch (switchValue) {
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println("Letter " + switchValue + " was not found in switch");
+                break;
+        }
+    }
+}
