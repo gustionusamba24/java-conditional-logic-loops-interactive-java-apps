0e2ea48f280a3c7b43c3de8e15007e50e7d83ddb
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-10-08 13:04:38 +0700

    feat(sum-3-and-5-challenge): finish the sum 3 and 5 challenge with Intellij IDEA debugger

---
+++ Sum3And5Challenge/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int countOfMatches = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                countOfMatches++;
+                sum += i;
+                System.out.println("Found number: " + i);
+            }
+
+            if (countOfMatches == 5) {
+                break;
+            }
+        }
+
+        System.out.println("The sum of found numbers is: " + sum);
+    }
+}
