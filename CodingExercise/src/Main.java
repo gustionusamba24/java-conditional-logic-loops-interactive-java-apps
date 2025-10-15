75ad054a6226d72da432b1fd3c9b9a8b6b8ccf14
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-10-15 20:59:37 +0700

    feat(coding-challenge-15): finish challenge part 15

--- CodingExercise/src/Main.java
+++ CodingExercise/src/Main.java
@@ -1,9 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import exercises.CE15SumOddRange;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
+        System.out.println("CE15SumOddRange Class");
+        System.out.println("Method: sumOdd(int start, int end)");
+        System.out.println();
+
+        System.out.println("sumOdd(1, 100) = " + CE15SumOddRange.sumOdd(1, 100));
+        System.out.println("sumOdd(-1, 100) = " + CE15SumOddRange.sumOdd(-1, 100));
+        System.out.println("sumOdd(100, 100) = " + CE15SumOddRange.sumOdd(100, 100));
+        System.out.println("sumOdd(13, 13) = " + CE15SumOddRange.sumOdd(13, 13));
+        System.out.println("sumOdd(100, -100) = " + CE15SumOddRange.sumOdd(100, -100));
+        System.out.println("sumOdd(100, 1000) = " + CE15SumOddRange.sumOdd(100, 1000));
     }
 }
---
+++ CodingExercise/src/exercises/CE15SumOddRange.java
@@ -0,0 +1,33 @@
+package exercises;
+
+public class CE15SumOddRange {
+
+    public static boolean isOdd(int number) {
+
+        if (number <= 0) {
+            return false;
+        }
+
+        return number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end) {
+
+        if (end < start) {
+            return -1;
+        }
+
+        int sum = 0;
+        if ((start > 0) && (end > 0)) {
+            for (int i = start; i <= end; i++) {
+                if (isOdd(i)) {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        return -1;
+    }
+}
